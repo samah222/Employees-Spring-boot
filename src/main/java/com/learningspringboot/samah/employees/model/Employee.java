ea347eeab9849827a17ad86b729c0e1b4bf845e1
Samah Mahdi <samahmahdi22@gmail.com>
2024-01-03 18:41:25 +0400

    improvements for clean code

--- src/main/java/com/learningspringboot/samah/employees/model/Employee.java
+++ src/main/java/com/learningspringboot/samah/employees/model/Employee.java
@@ -19,23 +19,30 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long Id;
+
     @Size(min = 2, max=50, message = "name should be between 2 and 50 characters")
     @Column(name = "name", nullable = false )
     private String name;
 
     @Min(1000)
     private double salary;
+
     @Email
     //@Column(unique = true)
     private String email;
+
     @Pattern(regexp ="^\\+[0-9]+$")
     private String phone;
+
     private String jobTitle;
+
     @NotBlank(message = "Department name is mandatory field")
     private String department;
+    
     @CreationTimestamp
     @Column(name="created_at")
     private Date createdAt;
+    
     @UpdateTimestamp
     @Column(name="updated_at", nullable = false, updatable = false)
     private Date updatedAt;
